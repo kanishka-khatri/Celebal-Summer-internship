c09fd59957064cb7a7874f02e723aad0516cf97f
Kanishka Khatri <khatrikanishka9@gmail.com>
2025-06-28 12:33:05 +0530

    Assignment-week4

---
+++ Celebal-Week4-express-server/server.js
@@ -0,0 +1,36 @@
+// Import express
+const express = require('express');
+
+// Create an express app
+const app = express();
+
+// Middleware to log the request method and URL
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Route 1: Home route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Home Page!');
+});
+
+// Route 2: About route
+app.get('/about', (req, res) => {
+  res.send('This is the About Page.');
+});
+
+// Route 3: POST route example
+app.post('/submit', (req, res) => {
+  const data = req.body;
+  res.send(`Data received: ${JSON.stringify(data)}`);
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
